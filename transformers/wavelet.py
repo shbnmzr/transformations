c25ee02ee53fa2cab752c60d34930b1a37478f0f
shabnamzareshahraki <shabnam.zareshahraki@studenti.unipd.it>
2025-11-18 20:02:14 +0100

    creates the class for Wavelet

---
+++ transformers/wavelet.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class HaarWaveletTransform(nn.Module):
+    """
+    minimal differentiable wavelet transform
+    """
+    def __init__(self, mode):
+        super().__init__()
+        assert mode in ['1d', '2d']
+        self.mode = mode
+
+        # Haar low-pass and high-pass filters
+        h = torch.tensor([1 / torch.sqrt(torch.tensor(2.0)),
+                          1 / torch.sqrt(torch.tensor(2.0))])
+
+        g = torch.tensor([1 / torch.sqrt(torch.tensor(2.0)),
+                          1 / torch.sqrt(torch.tensor(2.0))])
+
+        self.register_buffer('h', h.view(1, 1, -1))
+        self.register_buffer('g', g.view(1, 1, -1))

fc738ea31db890fdf7087d3f9cb52045c2550272
shabnamzareshahraki <shabnam.zareshahraki@studenti.unipd.it>
2025-11-18 19:51:47 +0100

    implements fourier 12 transform

--- transformers/fourier.py
+++ transformers/fourier.py
@@ -1,10 +1,19 @@
 import torch
 
 
-def fourier_1D(signal: torch.Tensor) -> torch.Tensor:
+def fourier_1d(signal: torch.Tensor) -> torch.Tensor:
     """
     computes the Fourier transform of a 1D signal
     :param signal: 1D signal, which is a Tensor of shape (...,N)
     :return: complex-valued Fourier coefficients
     """
     return torch.fft.fft(signal)
+
+
+def fourier_2d(signal: torch.Tensor) -> torch.Tensor:
+    """
+    computes the Fourier transform of a 2D signal
+    :param signal: Tensor of shape (B, C, H, W)
+    :return: complex-valued Fourier coefficients
+    """
+    return torch.fft.fft2(signal)
